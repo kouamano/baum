60e957a61aef42dbcdbf880de83dc28e6664c096
kouamano <amano.au1@gmail.com>
2013-09-13 15:22:40 +0900

    revise

---
+++ test/skelton.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*- 
+
+import sys
+import xml.dom.minidom
+
+EOD=";;;"
+
+argvs=sys.argv
+
+def getchild(root, level):
+	print ":"*level + "<" + root.tagName + ">"
+	for node in root.childNodes:
+		if node.nodeType == node.ELEMENT_NODE:
+			i = level + 1
+			getchild(node, i)
+
+
+
+
+if __name__ == "__main__":
+	print argvs[1]
+	dom = xml.dom.minidom.parse(argvs[1])
+	getchild(dom.documentElement, 0)
+	print EOD
+#	for url in dom.getElementsByTagName("url"):
+#		print url.firstChild.data
